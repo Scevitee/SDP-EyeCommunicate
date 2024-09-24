9bad4a2eedd8670243f2dcb2fd4df0ad76e2aea5
the0123 <amdetheodros@gmail.com>
2024-09-24 18:43:58 -0400

    Updated comments in testeye.py
    Fixed verbiage to refer to 'render_boxes_with_info' as a modified function and not a custom one.

--- eyetracking/testeye.py
+++ eyetracking/testeye.py
@@ -1,6 +1,6 @@
 # import the InferencePipeline interface
 from inference import InferencePipeline
-# import a built in sink called render_boxes (sinks are the logic that happens after inference)
+# import a modified version of 'render_boxes'
 from eyecoordinates import render_boxes_with_info
 # importing os module for environment variables
 import os
@@ -13,7 +13,7 @@ load_dotenv()
 pipeline = InferencePipeline.init(
     model_id="eye-tracking-2l0uc/5",  # set the model id to a yolov8x model with in put size 1280
     video_reference=0,  # set the video reference (source of video), it can be a link/path to a video file, an RTSP stream url, or an integer representing a device id (usually 0 for built in webcams)
-    on_prediction=render_boxes_with_info,  # Use the custom function to handle each set of inferences
+    on_prediction=render_boxes_with_info,  # Use the modified version of 'render_boxes' to handle each set of inferences
     api_key=os.getenv("API_KEY"),  # provide your roboflow api key for loading models from the roboflow api
 )
 
