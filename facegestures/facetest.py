30b4a84111dadd4a375f33411a3b9d217976ae56
Justin Darcey <91916142+Scevitee@users.noreply.github.com>
2024-10-08 19:34:35 -0400

    changes

--- facegestures/facetest.py
+++ facegestures/facetest.py
@@ -60,16 +60,20 @@ BUFFER_DURATION = 20
 bct = 0
 calibrated_ear = 0
 ear_list = []
-calibration_time = 5  # seconds
+calibration_time = 2  # seconds
 start_time = time.time()
 
 calibrating = True
-while True:
+# Define a variable to keep track of the frames to display the message
+blink_display_frames = 0
+BLINK_DISPLAY_DURATION = 30  # Number of frames to show the "Blink detected" message
 
+while True:
     frame = cap.read()
     frame = imutils.resize(frame, width=800)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.bilateralFilter(gray, 7, 20, 20)
+    
     # Detect faces in the frame
     faces = detector(gray, 0)
 
@@ -81,39 +85,36 @@ while True:
 
         # check if in the calibration period of program
         if calibrating:
-
             cv2.putText(frame, "Calibrating", (x // 3, (y - (y // 2))), 2, 2, 1, 2)
             ear = calculate_ear(landmarks)
             ear_list.append(ear)
 
             if (time.time() - start_time) > calibration_time:
-                # choosing a low percentile instead of min() to protect against
-                # edge case of a blink during the calibration period
                 calibrated_ear = 0.76 * np.percentile(ear_list, 5)
                 print(f"Calibrated EAR: {calibrated_ear}")
                 calibrating = False
 
         else:
-
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
 
             for (i, (x, y)) in enumerate(landmarks):
                 # draw circles at the position of the facial landmarks
                 cv2.circle(frame, (x, y), 1, (0, 255, 0), 1)
 
-                # draw the number corresponding the landmark position
-                # cv2.putText(frame, f"{i + 1}", (x, y), 1, .5, 1, 1)
-
             # Check for specific gestures
             blink_detected, buffer_frames, blink_queue = detect_blink(landmarks, calibrated_ear, blink_queue, buffer_frames, BUFFER_DURATION)
             if blink_detected:  
                 bct += 1
-                print(f"Blink count: {bct}")
-
+                # Set the blink display frames to the specified duration
+                blink_display_frames = BLINK_DISPLAY_DURATION
 
             if detect_eyebrow_raise(landmarks):
-                # print('Eyebrow raise detected!')
                 eyebrow_counter = 0
+
+    # Display "Blink detected" on the frame for a few frames
+    if blink_display_frames > 0:
+        cv2.putText(frame, "Blink detected", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        blink_display_frames -= 1
 
     # Display the frame for debugging
     cv2.imshow('Facial Gesture Control', frame)
@@ -122,6 +123,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# 6. Clean up
+# Clean up
 cv2.destroyAllWindows()
 cap.stop()
